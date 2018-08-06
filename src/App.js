b7b6a3f81ae06ba913292178012bdb11916e7ff9
Marek Gacek <gacekmarek92@gmail.com>
2018-08-06 21:32:08 +0200

    Search methods added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from "./BookList";
 import {Route, Link} from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+     books: [],
+     query: '',
+     showingBooks: []
+   }
 
+   // get all the books before loading the component
+     componentDidMount() {
+       BooksAPI.getAll().then((books) => {
+         this.setState({books})
+       })
+     }
+     // managing the input state
+  updateQuery = (query) => {
+    this.setState({query: query})
+    let showingBooks = []
+    if (query) {
+      BooksAPI.search(query).then(response => {
+        if (response.length) {
+          showingBooks = response.map(book => {
+            const index = this.state.books.findIndex(c => c.id === book.id)
+            if( index >= 0 ) {
+              return this.state.books[index]
+            } else {
+              return book
+            }
+          })
+        }
+        this.setState({showingBooks})
+      })
+    }
+    else {
+      this.setState({showingBooks})
+    }
+  }
   render() {
+    const {query} = this.state
     return (
       <div className="app">
       <Route exact path="/search" render={() => (
@@ -31,12 +57,44 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text"
+                placeholder="Search by title or author"
+                value={query}
+                onChange={(event) => this.updateQuery(event.target.value)}
+                />
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+              {this.state.showingBooks.map((book, i) => (
+                    <li key={i}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{
+                            width: 128,
+                            height: 192,
+                            backgroundImage: book.imageLinks ?
+                              `url(${book.imageLinks.thumbnail})` : ''
+                          }}></div>
+                          <div className="book-shelf-changer">
+                            <select onChange={this.changeBookShelf} value={this.state.shelf}>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading
+                              </option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors ? book.authors.toString() : ' '}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+
             </div>
           </div>
         )} />
